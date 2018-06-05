3c719807e54f55c1ec681bf00b9930968e826d8a
alteste <james@Jamess-MacBook-Pro-3.local>
2018-06-06 10:21:20 +1200

    Added variables and have text field and slider respond with print lines

--- TipsyTipCalculator/Controller/ViewController.swift
+++ TipsyTipCalculator/Controller/ViewController.swift
@@ -10,16 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var enterBillTF: UITextField!
+    @IBOutlet weak var tipPercentLbl: UILabel!
+    @IBOutlet weak var tipPercentSlider: UISlider!
+    @IBOutlet weak var tipAmountLbl: UILabel!
+    @IBOutlet weak var totalAmountLbl: UILabel!
+    
+    var tipCalcBrains = TipCalcBrains(billAmount: 25.00, tipPercent: 0.15)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func billAmountChange(_ sender: Any) {
+        print("bill has changed")
     }
-
-
+    
+    @IBAction func tipPercentChange(_ sender: Any) {
+        print("tip has changed")
+    }
+    
 }
 
