5206c6ce95ce254dcd2207032606870d14206881
alteste <james@Jamess-MacBook-Pro-3.local>
2018-06-06 09:38:33 +1200

    Started project and completed UI

---
+++ TipsyTipCalculator/Model/TipCalcBrains.swift
@@ -0,0 +1,9 @@
+//
+//  TipCalcBrains.swift
+//  TipsyTipCalculator
+//
+//  Created by James Campbell on 6/06/18.
+//  Copyright © 2018 James Campbell. All rights reserved.
+//
+
+import Foundation
